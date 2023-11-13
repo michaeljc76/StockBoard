75c49b251c07918d47835f73084ded050da729ce
Michael Crowley <nico404.site@gmail.com>
2023-11-13 10:55:35 -0500

    Added dark theme for chart and cards

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { mockHistoricalData } from '../constants/mock'
 import { unixToDate, dateToUnix, createDate } from '../helpers/datehelper';
 import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import Card from './Card';
 import { chartConfig } from '../constants/config';
 import ChartFilter from './ChartFilter';
+import ThemeContext from '../context/ThemeContext';
 
 const Chart = () => {
     const [data, setData] = useState(mockHistoricalData);
@@ -19,6 +20,7 @@ const Chart = () => {
         });
     };
 
+    const { darkMode } = useContext(ThemeContext);
   return (
     <Card>
         <ul className='flex absolute top-2 right-2 z-40'>
@@ -36,11 +38,11 @@ const Chart = () => {
             <AreaChart data={formatData(data)}>
                 <Area type='monotone' dataKey='value' stroke='#312e81' fillOpacity={1} strokeWidth={0.5} fill='url(#chartColor)'/>
                 <Tooltip />
-                <XAxis dataKey={"date"}/>
-                <YAxis domain={["dataMin", "dataMax"]} />
+                <XAxis dataKey={"date"} stroke={darkMode ? '#f7f7f7' : 'gray'} />
+                <YAxis domain={["dataMin", "dataMax"]} stroke={darkMode ? '#f7f7f7' : 'gray'}/>
                 <defs>
                     <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#ff3856" stopOpacity={0.8}/>
+                    <stop offset="5%" stopColor={darkMode ? '#ff667d': '#ff3856'} stopOpacity={0.8}/>
                     <stop offset="95%" stopColor="#ff3856" stopOpacity={0}/>
                     </linearGradient>
                 </defs>
--- src/components/LightDark.js
+++ src/components/LightDark.js
@@ -11,7 +11,7 @@ const LightDark = () => {
 
   return (
     <button className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg 
-    ${darkMode ? 'shadow-gray-100' : null}`}
+    ${darkMode ? 'shadow-gray-800' : null}`}
     onClick={toggleDarkMode}>
 
         <MoonIcon className={`h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400
